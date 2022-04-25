1d5b1b0b1780683647fe12d9f470ebe238a148a9
Machuka Joy <machukajoy@gmail.com>
2022-04-25 19:27:16 +0300

    ft(credentials class): create initial credentials class

--- passlocker.py
+++ passlocker.py
@@ -14,3 +14,22 @@ class User:
     self.password = password 
 
   pass
+
+#Credentials class
+class Credentials :
+  """
+  Class that creates new instances of credentials
+  """
+  credentials_list = [] # Empty credentials list
+
+  def __init__(self,account,username,password):
+
+    """
+    __init__ method that helps us define properties for our objects.
+    Args:
+      username : New user username.
+      password : New user password.
+    """
+    self.account = account
+    self.username = username
+    self.password = password
