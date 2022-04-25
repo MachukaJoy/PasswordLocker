87f342bb6d6fb3156fce8fe8e882412ecf60e44f
Machuka Joy <machukajoy@gmail.com>
2022-04-25 16:29:46 +0300

    ft: add method to define user ibject properties

--- passlocker.py
+++ passlocker.py
@@ -1,3 +1,16 @@
 class User:
+  user_list = [] # Empty user list
+
+  def __init__(self,username,password):
+
+    """
+    __init__ method that helps us define properties for our objects.
+    Args:
+        username : New user username.
+        password : New user password.
+    """
+
+    self.username = username
+    self.password = password 
 
   pass
