11cadb5fa60eb0a13e20f232e7ea3f34c98e79d2
Machuka Joy <machukajoy@gmail.com>
2022-04-26 10:02:15 +0300

    ft(create acc main method): add the create account option to the main method

--- run.py
+++ run.py
@@ -49,15 +49,41 @@ def display_credentials():
 # create main function
 def main() :
   print("Hi!This is Password Manager.What is your name")
-  my_name = input()
+  user_name = input()
 
-  print(f"Great ! What would you like to do { my_name }?")
+  print(f"Great ! What would you like to do { user_name }?")
   print('\n')
 
-  while True:
-    print("Choose a short code : ca - create a new credential, dc - display credentials, fc -find credential, ex -exit credential list ")
-
-    short_code = input().lower()
+  while True: 
+      print("Lets create an account or Login to proceed or exit. Use IN to login, CA to Create Account or EXIT to exit application ")
+      # print("lets proceed use IN or CA")
+      user_input =input()
+      if user_input =="CA":
+          print("Proceed to create your password Locker Account")
+          print("--"*20)
+          print("Enter your First Name")
+          fname = input()
+          print("Enter your Last Name")
+          sname = input()
+          print("Enter your login key")
+          loginkey = input()
+    
+          #Saving the user created credentials
+          save_user(create_user(fname, sname, loginkey))
+          print("Your password-locker account created successfully...please proceed to login")
+          print("--" *20)
+          print(f"FullNames -> {fname}{sname} \n User Name -> {user_name} \nloginkey/loginkey -> {loginkey}")
+          print("|     ___   ____      _   ")
+          print("|    |   | |  __  |  | \  |")
+          print("|___ |___| |___|  |  |  \_|")
+          print("Please provide your user Name")
+          usernameaccount = input()
+          print("Provide your loginkey")
+          loginkey = input()
+          if usernameaccount == user_name and loginkey ==loginkey:
+              print("Successfully Logged In..welcome.......")
+              print("**"*10)
+          pass
 
 
 if __name__ == '__main__':
